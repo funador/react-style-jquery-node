0d6c8b8e621ee670b9fe04c83ea5737b0ad6e05a
funador <jesse.heaslip@gmail.com>
2018-02-08 21:05:44 -0600

    adding favicon

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ app.use(bodyParser.json())
 // route all calls to /api to todoRouter
 app.use('/api', todoRouter)
 
-// ignore the favicon
-app.get('/favicon.ico', (req, res) => {
-    res.status(204);
-})
-
 // catch everything that doesn't go to the api
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
